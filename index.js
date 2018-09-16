de1247e1f00eb86c994600718d77a96c5ed09ff9
devwiltbank <devin.wiltbank@gmail.com>
2018-09-16 16:03:30 -0700

    initial files

---
+++ index.js
@@ -0,0 +1,21 @@
+// Temporary practice coding here...
+
+// function sayHello() {
+//     for (var i = 0; i < 5; i++) {
+//       console.log(i);
+//     }
+//     console.log(i);
+//   }
+
+// ES5 old regular style function
+var multiplyES5 = function(x, y) {
+  return x * y;
+};
+console.log(multiplyES5(3, 4));
+
+// Arrow function in ES6
+// const multiplyES6 = (x, y) => {
+//   return x * y;
+// };
+// sayHello(3, 4);
+
